a7de31d0bfddf8e3d107d6a1aabca9bd453cbc78
sgalabov02 <91763598+sgalabov02@users.noreply.github.com>
2022-06-20 14:28:36 +0300

    Add files via upload

---
+++ Lab/01. Hello World/Hello World.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int age;
+    printf("Hello World!\n");
+    printf("Hello C Programming\n");
+
+    printf("Enter your age: ");
+    scanf("%d", &age);
+
+    printf("Your age is: %d", age);
+    return 0;
+}
