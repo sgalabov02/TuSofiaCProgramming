b533646299c81b009cc0012a33826b085911d2c5
sgalabov02 <91763598+sgalabov02@users.noreply.github.com>
2022-06-20 14:48:29 +0300

    Add files via upload

---
+++ Lab/03. If else/01. Number Pyramid.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, j, rows;
+    printf ("Number of rows: ");
+    scanf("%d", &rows);
+    for (i = 1; i <= rows; i++)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            printf("%d ", i);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
